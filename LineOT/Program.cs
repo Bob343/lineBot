6aa9722133dc7b1fba1e89a9db38ca7ee69e00dd
root <root@DESKTOP-41GSSQH.dhcp.hofstra.edu>
2017-04-06 14:45:26 -0400

    Initializing

---
+++ LineOT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Thrift.Transport;
+using Thrift.Protocol;
+using Thrift.Collections;
+using Thrift.Server;
+using System.Threading.Tasks;
+
+namespace LineOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transport = new TSocket("localhost", 9090);
+        }
+    }
+}
